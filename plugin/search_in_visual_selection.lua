b350d72c9c2e4c0ea6eaee3eed39ece407359162
Bagohart <bagohart@gmx.de>
2023-02-04 22:16:18 +0100

    fixed autoloading

--- plugin/search_in_visual_selection.lua
+++ plugin/search_in_visual_selection.lua
@@ -1,1 +1,1 @@
-vim.keymap.set('x', '<Plug>(search-in-visual-selection)', function() return require('search_in_visual_selection').search_in_visual_selection end, { expr = true })
+vim.keymap.set('x', '<Plug>(search-in-visual-selection)', function() return require('search_in_visual_selection').search_in_visual_selection() end, { expr = true })
